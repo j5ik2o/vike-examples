9099741eea562437d36624f98fe99455e6357a5c
Junichi Kato <j5ik2o@gmail.com>
2023-08-14 06:44:27 +0900

    wip

--- boilerplates/boilerplate-vue-js/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reportUnusedDisableDirectives: true,
-  ignorePatterns: ["dist/*"],
-  env: { browser: true, es2020: true, node: true },
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  extends: ["eslint:recommended"],
-};
