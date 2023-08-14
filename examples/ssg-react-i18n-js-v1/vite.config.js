95038146f3b09291a7fa94e7e19860d4f2d3b592
Junichi Kato <j5ik2o@gmail.com>
2023-08-15 04:14:37 +0900

    wip

--- examples/ssg-react-i18n-js-v1/vite.config.js
+++ examples/ssg-react-i18n-js-v1/vite.config.js
@@ -2,6 +2,11 @@ import react from "@vitejs/plugin-react";
 import ssr from "vite-plugin-ssr/plugin";
 
 export default {
+  root: "./src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
   plugins: [
     react(),
     ssr({
