c8e8726d753639a5a42bfc4fabce98f2e0305f83
Junichi Kato <j5ik2o@gmail.com>
2023-08-27 08:11:19 +0900

    wip

--- examples/ts-ssr-react-next-auth-1.0/src/pages/index/+Page.tsx
+++ examples/ts-ssr-react-next-auth-1.0/src/pages/index/+Page.tsx
@@ -2,19 +2,21 @@ import { useSession } from "next-auth/react";
 function Page() {
   const { data: session, status } = useSession();
 
-  console.log("session", session);
-  console.log("status", status);
-  const userEmail = session?.user?.email;
-
   if (status === "authenticated") {
-    return <p>Signed in as {userEmail}</p>;
+      const userEmail = session?.user?.email;
+    return (
+       <>
+           <p>Signed in as {userEmail}</p>
+           <br />
+           <a href="/api/auth/signout">Sign out</a>
+       </>
+    );
   }
 
   return (
     <>
       <a href="/api/auth/signin">Sign in</a>
       <br />
-      <a href="/api/auth/signout">Sign out</a>
     </>
   );
 }
