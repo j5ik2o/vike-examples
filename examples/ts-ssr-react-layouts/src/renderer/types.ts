6739b23df721b911f2e587794b713f1bbabfae25
Junichi Kato <j5ik2o@gmail.com>
2023-09-02 09:08:50 +0900

    update vite-plugin-ssr

--- examples/ts-ssr-react-layouts/src/renderer/types.ts
+++ examples/ts-ssr-react-layouts/src/renderer/types.ts
@@ -1,12 +1,7 @@
 import React from "react";
 import type {
-  PageContextBuiltIn,
-  /*
-// When using Client Routing https://vite-plugin-ssr.com/clientRouting
-PageContextBuiltInClientWithClientRouting as PageContextBuiltInClient
-/*/
-  // When using Server Routing
   PageContextBuiltInClientWithServerRouting as PageContextBuiltInClient,
+  PageContextBuiltInServer,
 } from "vite-plugin-ssr/types";
 
 type PageProps = Record<string, unknown>;
@@ -15,16 +10,16 @@ type Page = (pageProps: PageProps) => React.ReactElement;
 type PageContextCustom = {
   Page: Page;
   pageProps?: PageProps;
-  urlPathname: string;
-  exports: {
-    documentProps?: {
-      title?: string;
-      description?: string;
-    };
-  };
+  // urlPathname: string;
+  // exports: {
+  //   documentProps?: {
+  //     title?: string;
+  //     description?: string;
+  //   };
+  // };
 };
 
-type PageContextServer = PageContextBuiltIn<Page> & PageContextCustom;
+type PageContextServer = PageContextBuiltInServer<Page> & PageContextCustom;
 type PageContextClient = PageContextBuiltInClient<Page> & PageContextCustom;
 
 type PageContext = PageContextClient | PageContextServer;
